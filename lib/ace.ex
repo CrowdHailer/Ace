7a6d8dc0f26a9f84a423e567b264eb8c1c579a16
CrowdHailer <peterhsaxton@gmail.com>
2016-10-13 12:06:13 +0100

    remove obsolete example code

--- lib/ace.ex
+++ lib/ace.ex
@@ -1,21 +1,3 @@
-defmodule MyServer do
-  def init(_connection, state = {:greeting, greeting}) do
-    {:send, greeting, state}
-  end
-
-  def handle_packet(inbound, state) do
-    {:send, "ECHO: #{String.strip(inbound)}\r\n", state}
-  end
-
-  def handle_info({:notify, notification}, state) do
-    {:send, "#{notification}\r\n", state}
-  end
-
-  def terminate(_reason, _state) do
-    IO.puts("Socket connection closed")
-  end
-end
-
 defmodule Ace do
   use Application
 
