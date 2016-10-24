e5ee88d1128af09b6e6c7d54dc6a3bac96d46cb9
CrowdHailer <peterhsaxton@gmail.com>
2016-10-24 17:34:31 +0100

    use new options in QOTD example

--- examples/01_quote_of_the_day/lib/quote_of_the_day.ex
+++ examples/01_quote_of_the_day/lib/quote_of_the_day.ex
@@ -5,14 +5,14 @@ defmodule QuoteOfTheDay do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(Ace.TCP, [{__MODULE__, []}, [port: 17]])
+      worker(Ace.TCP, [{__MODULE__, []}, [port: 17, name: QuoteOfTheDay.TCP, acceptors: 5]])
     ]
 
     opts = [strategy: :one_for_one, name: QuoteOfTheDay.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  def init(conn, state) do
+  def init(_conn, state) do
     {:send, "The future is here, just unevenly distributed", state}
   end
 end
