c6d7abcfd5c090d210a6d76ad0856c337f9b7656
CrowdHailer <peterhsaxton@gmail.com>
2017-04-16 16:04:06 +0100

    move ace_http as part of this project

---
+++ extensions/http(s)/mix.exs
@@ -0,0 +1,42 @@
+defmodule Ace.HTTP.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :ace_http,
+     version: "0.3.0",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps(),
+     description: description(),
+     docs: [extras: ["README.md"], main: "readme"],
+     package: package()]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:raxx, "~> 0.11.0"},
+      {:http_status, "~> 0.2.0"},
+      {:ace, "~> 0.9.0"},
+      {:raxx_verify, "~> 0.1", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    HTTP Server built on top of Ace TCP connection manager
+    """
+  end
+
+  defp package do
+    [
+     maintainers: ["Peter Saxton"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/CrowdHailer/raxx/tree/master/ace_http"}]
+  end
+end
