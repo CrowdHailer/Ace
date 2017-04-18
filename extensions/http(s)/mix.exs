bd92a7810158c91c1ccd43aa0bd9c81110f372ba
CrowdHailer <peterhsaxton@gmail.com>
2017-04-18 12:53:32 +0100

    release http with chunked response

--- extensions/http(s)/mix.exs
+++ extensions/http(s)/mix.exs
@@ -3,7 +3,7 @@ defmodule Ace.HTTP.Mixfile do
 
   def project do
     [app: :ace_http,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
