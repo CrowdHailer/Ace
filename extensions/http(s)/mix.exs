113f26492523589c50eff0a10ad21f5932aa05c6
CrowdHailer <peterhsaxton@gmail.com>
2017-05-07 15:07:17 +0100

    release with improved errors

--- extensions/http(s)/mix.exs
+++ extensions/http(s)/mix.exs
@@ -3,7 +3,7 @@ defmodule Ace.HTTP.Mixfile do
 
   def project do
     [app: :ace_http,
-     version: "0.4.3",
+     version: "0.4.4",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
