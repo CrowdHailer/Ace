d7eb660f9cfc8bfd8d0f2a049fa42ec6d395f25a
CrowdHailer <peterhsaxton@gmail.com>
2017-06-28 01:24:25 +0100

    further tests for frame processing

---
+++ test/ace/frame_test.exs
@@ -0,0 +1,131 @@
+defmodule Ace.Frame do
+  def pop(binary = <<l::24, _::48, _::binary-size(l)>> <> rest) do
+    IO.inspect(l)
+    # <<frame::binary-size(l + 9), rest::bits>> = binary
+    # {frame, rest}
+  end
+  def parse do
+
+  end
+  # def read_next(<<header::bits-size(72), rest :: bits>>) do
+  #   <<length::24, _::bits>> = header
+  #   <<payload::binary-size(length), _::bits>> = rest
+  # end
+  defmodule Data do
+    defstruct [data: "", padding: 0, end: false]
+    def parse_flags(<<_::5, padded_flag::1, _::1, end_stream_flag::1>>) do
+      %{
+        padded: padded_flag == 1,
+        end_stream_flag: end_stream_flag == 1,
+      }
+    end
+
+    def parse_payload(payload, length, true) do
+
+    end
+
+
+  end
+  # <<length::24, Data.type(), Data.flags(:padded, end_stream_flag), 0::1, id:31>>
+  defmodule WindowUpdate do
+    defmacro type do
+      <<8>>
+    end
+  end
+end
+defmodule Ace.FrameTest do
+
+  use ExUnit.Case
+
+  @priority <<2>>
+  @settings <<4>>
+  @window_update <<8>>
+
+  def parse_flags(<<0>>) do
+    %{ack: false}
+  end
+  def parse_flags(<<1>>) do
+    %{ack: true}
+  end
+
+  def parse_frame(<<@priority, flags :: bits - size(8), 0 :: 1, stream_id :: 31>>, payload) do
+    flags = parse_priority(flags, payload)
+    {:settings, flags}
+  end
+  def parse_frame(<<@settings, flags :: bits - size(8), 0 :: 1, 0 :: 31>>, payload) do
+    flags = parse_settings(flags, payload)
+    {:settings, flags}
+  end
+  def parse_frame(<<@window_update, flags :: bits - size(8), 0 :: 1, 0 :: 31>>, payload) do
+    flags = parse_window_update(flags, payload)
+    {:settings, flags}
+  end
+
+  def parse_priority(<<0>>, <<0::1, stream_dependency::31, weight::8>>) do
+    IO.inspect(stream_dependency)
+  end
+
+  def parse_settings(<<1>>, "") do
+    :ack
+  end
+  def parse_settings(<<0>>, payload) do
+    parse_parameters(payload)
+  end
+
+  def parse_parameters(binary, parameters \\ %{})
+  def parse_parameters(<<>>, parameters) do
+    parameters
+  end
+  def parse_parameters(<<1 :: 16, value :: 32, rest :: binary>>, parameters) do
+    IO.inspect(value)
+    parse_parameters(rest)
+  end
+  def parse_parameters(<<4 :: 16, value :: 32, rest :: binary>>, parameters) do
+    IO.inspect(value)
+    parse_parameters(rest)
+  end
+  def parse_parameters(<<5 :: 16, value :: 32, rest :: binary>>, parameters) do
+    IO.inspect(value)
+    parse_parameters(rest)
+  end
+
+  def parse_window_update(<<0>>, <<0 :: 1, window_size_increment :: 31>>) do
+    IO.inspect(window_size_increment)
+  end
+
+  def read_frame(<<length :: 24, head :: binary - size(6), rest :: bits>>) do
+    <<payload :: binary - size(length), rest :: bits>> = rest
+    {{head, payload}, rest}
+  end
+
+  test "" do
+    read_frame(<<0 :: 24, 4 :: 8, 0 :: 8, 0 :: 1, 0 :: 31>>)
+    |> IO.inspect
+  end
+
+  test "firefox 1" do
+    {{head, payload}, ""} = read_frame(<<0, 0, 18, 4, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 4, 0, 2, 0, 0, 0, 5, 0, 0,
+  64, 0>>)
+    parse_frame(head, payload)
+  end
+  test "firefox 2" do
+    {{head, payload}, ""} = read_frame(<<0, 0, 4, 8, 0, 0, 0, 0, 0, 0, 191, 0, 1>>)
+    parse_frame(head, payload)
+  end
+  test "firefox 3" do
+    {{head, payload}, ""} = read_frame(<<0, 0, 5, 2, 0, 0, 0, 0, 3, 0, 0, 0, 0, 200>>)
+    parse_frame(head, payload)
+  end
+  test "firefox 4" do
+    <<0, 0, 5, 2, 0, 0, 0, 0, 5, 0, 0, 0, 0, 100>>
+  end
+  test "firefox 5" do
+    <<0, 0, 5, 2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0>>
+  end
+  test "firefox 6" do
+    <<0, 0, 5, 2, 0, 0, 0, 0, 9, 0, 0, 0, 7, 0>>
+  end
+  test "firefox 7" do
+    <<0, 0, 5, 2, 0, 0, 0, 0, 11, 0, 0, 0, 3, 0>>
+  end
+end
--- test/ace_test.exs
+++ test/ace_test.exs
@@ -4,10 +4,53 @@ defmodule AceTest do
     # Setup a server
     {:ok, %{port: :port}}
   end
+  @preface "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+
+  def parse_frame(<<length :: 24, type :: 8, flags :: 8, 0 :: 1, id :: 31>> <> rest) do
+    # IO.inspect(length)
+    # IO.inspect(id)
+    # IO.inspect(type)
+    # IO.inspect(rest)
+    <<payload :: binary - size(length), rest :: bitstring>> = rest
+    {type, payload, rest}
+  end
+
+  def loop(socket, buffer \\ "") do
+    :ssl.setopts(socket, [active: :once])
+    IO.inspect(buffer)
+    {_, _, rest} =parse_frame(buffer)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    {_, _, rest} = parse_frame(rest)
+    |> IO.inspect
+    receive do
+      {:ssl, ^socket, @preface <> data} ->
+        parse_frame(data)
+        |> IO.inspect
+        IO.inspect(data)
+        loop(socket)
+    end
+  end
 
   test "get a single response" do
-    preface = "PRI * HTTP/2.0\r\n\r\nSM: Value\r\n\r\n"
-
+    preface = @preface
     Application.ensure_all_started(:crypto)
     |> IO.inspect
     certfile =  Path.expand("./ace/tls/cert.pem", __DIR__)
@@ -28,27 +71,37 @@ defmodule AceTest do
       {:ok, socket} = :ssl.transport_accept(listen_socket)
       IO.puts("Server accepted")
       :ok = :ssl.ssl_accept(socket)
-      IO.inspect(:ssl.negotiated_protocol(socket))
+      {:ok, "h2"} = :ssl.negotiated_protocol(socket)
+      IO.inspect("connected over http2")
       :ssl.setopts(socket, [active: :once])
+      # Don't need to send preface immediatly
+      :ssl.send(socket, <<0 :: 24, 4 :: 8, 0 :: 8, 0 :: 1, 0 :: 31>>)
       receive do
-        data ->
-          IO.inspect(data)
+        {:ssl, ^socket, @preface <> buffer} ->
+          loop(socket, buffer)
+        # any ->
+        #   IO.inspect(any)
+        #   loop(socket, "")
+      after
+        1_000 ->
+          receive do
+            any ->
+              IO.puts("Fail")
+              IO.inspect(any)
+          end
       end
-      receive do
-        data ->
-          IO.inspect(data)
-      end
-      :timer.sleep(5_000)
     end)
     :timer.sleep(1_000)
-    {:ok, socket} = :ssl.connect('localhost', 8443, [
-      mode: :binary,
-      packet: :raw,
-      active: :false,
-      alpn_advertised_protocols: ["h2"]])
-    |> IO.inspect
-    IO.inspect(:ssl.negotiated_protocol(socket))
-    :ssl.send(socket, preface)
+    # {:ok, socket} = :ssl.connect('localhost', 8443, [
+    #   mode: :binary,
+    #   packet: :raw,
+    #   active: :false,
+    #   alpn_advertised_protocols: ["h2"]])
+    # |> IO.inspect
+    # IO.inspect(:ssl.negotiated_protocol(socket))
+    # :ssl.send(socket, preface <> <<4 :: 24, 4 :: 8, 0 :: 8, 0 :: 1, 0 :: 31>> <> "four")
+    # :ssl.recv(socket, 0, 1_000)
+    # |> IO.inspect
     Task.await(task, 60_000)
   end
 end
