27d687a503d1f9b056bd7b532c6a38f6b7cb7671
CrowdHailer <peterhsaxton@gmail.com>
2017-07-02 15:31:46 +0100

    separate frame code

--- lib/ace.ex
+++ lib/ace.ex
@@ -1,76 +1,125 @@
 defmodule Ace do
+  @preface "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+  @default_settings %{}
+
+  def preface() do
+    @preface
+  end
 
   defstruct [
-    settings: nil
+    settings: nil,
+    socket: nil
   ]
 
-  def start_link(port, options) do
-    spawn_link(__MODULE__, :ready, [])
+  use GenServer
+  def start_link(listen_socket) do
+    GenServer.start_link(__MODULE__, listen_socket)
   end
 
-  def ready do
-    receive do
-      {:"$gen_server", from, {:accept}}
+  def init(listen_socket) do
+    {:ok, {:listen_socket, listen_socket}, 0}
+  end
+  def handle_info(:timeout, {:listen_socket, listen_socket}) do
+    {:ok, socket} = :ssl.transport_accept(listen_socket)
+    IO.puts("Server accepted")
+    :ok = :ssl.ssl_accept(socket)
+    {:ok, "h2"} = :ssl.negotiated_protocol(socket)
+    :ssl.setopts(socket, [active: :once])
+    {:noreply, {:pending, %__MODULE__{socket: socket}}}
+  end
+  def handle_info({:ssl, _, @preface <> data}, {:pending, state}) do
+    consume(data, state)
+  end
+
+  def consume(buffer, state) do
+    {frame, unprocessed} = Ace.Frame.read_next(buffer) # + state.settings )
+    if frame do
+      {outbound, state} = consume_frame(frame, state)
+      :ok = :ssl.send(state.socket, outbound)
+      consume(unprocessed, state)
+    else
+      {:noreply, state}
     end
   end
 
-  def loop(state = %{socket: socket}) do
-    receive do
-      {ACE, frame} ->
-        {:ok, frames} = constrain_frame(frame, state.settings)
-        state = %{state | outbound: state.outbound ++ frames}
-      {:ssl, ^socket, data} ->
-        {buffer, state} = read_frames(buffer <> data, state)
-        loop(buffer, state)
-    end
+  def consume_frame(<<l::24, 4::8, 0::8, 0::1, 0::31, payload::binary>>, state = %{settings: nil}) do
+    new_settings = update_settings(payload)
+    {[<<0::24, 4::8, 1::8, 0::32>>], %{state | settings: new_settings}}
+  end
+  def consume_frame(_, state = %{settings: nil}) do
+    :invalid_first_frame
   end
 
-  def do_read_frames(buffer, state, socket) do
-    {pending, state} = read_frames(buffer, state)
-    expediate(pending, state, socket)
-    do_read_frames(buffer, state, socket)
+  def update_settings(new, old \\ @default_settings) do
+    %{}
   end
 
-  def read_frames(buffer, state) do
-    case Frame.pop(data) do
-      {:ok, {nil, buffer}} ->
-        {buffer, state}
-      {:ok, {frame, buffer}} ->
-        {pending, state} = handle_frame(frame, state)
-        read_frames(buffer, state)
-    end
-  end
-
-
-  def handle_frame(new = %Settings{}, %{settings: nil}) do
-    update_settings(new, nil)
-  end
-  def handle_frame(_, %{settings: nil}) do
-    # Unexpected frame for startup
-  end
-
-  def handle_frame(frame = %Headers{fin: true}, state) do
-    # start_stream(state.stream_supervisor)
-    {:ok, pid} = start_link(Ace.Stream, :init, [[frame]])
-    streams = Map.put(state.streams, frame.stream_id, pid)
-  end
-  def handle_frame(frame = %Headers{fin: false}, state) do
-    {[], %{state | stream_head: [frame]}}
-  end
-  def handle_frame(frame = %Continuation{fin: true}, state) do
-    stream_head = state.
-  end
-  def handle_frame(frame = %Data{}, state) do
-    {:ok, pid} = fetch_stream(frame, state)
-    Stream.send_data(pid, frame)
-  end
-
-  def start_stream(head = {:GET, "/foo", _ip}) do
-    # start under dynamic supervisor
-    Ace.FooController.start_link(head)
-    {:ok, pid} = start_link(Ace.FooController, :init, [[frame]])
-
-  end
+  #
+  # def ready do
+  #   receive do
+  #     {:"$gen_server", from, {:accept, socket}} ->
+  #       :ssl.accept
+  #   end
+  # end
+  #
+  # def loop(state = %{socket: socket}) do
+  #
+  #   receive do
+  #     {ACE, frame} ->
+  #       {:ok, frames} = constrain_frame(frame, state.settings)
+  #       state = %{state | outbound: state.outbound ++ frames}
+  #     {:ssl, ^socket, data} ->
+  #       {buffer, state} = read_frames(buffer <> data, state)
+  #       loop(buffer, state)
+  #   end
+  # end
+  #
+  # def do_read_frames(buffer, state, socket) do
+  #   {pending, state} = read_frames(buffer, state)
+  #   expediate(pending, state, socket)
+  #   do_read_frames(buffer, state, socket)
+  # end
+  #
+  # def read_frames(buffer, state) do
+  #   case Frame.pop(data) do
+  #     {:ok, {nil, buffer}} ->
+  #       {buffer, state}
+  #     {:ok, {frame, buffer}} ->
+  #       {pending, state} = handle_frame(frame, state)
+  #       read_frames(buffer, state)
+  #   end
+  # end
+  #
+  #
+  # def handle_frame(new = %Settings{}, %{settings: nil}) do
+  #   update_settings(new, nil)
+  # end
+  # def handle_frame(_, %{settings: nil}) do
+  #   # Unexpected frame for startup
+  # end
+  #
+  # def handle_frame(frame = %Headers{fin: true}, state) do
+  #   # start_stream(state.stream_supervisor)
+  #   {:ok, pid} = start_link(Ace.Stream, :init, [[frame]])
+  #   streams = Map.put(state.streams, frame.stream_id, pid)
+  # end
+  # def handle_frame(frame = %Headers{fin: false}, state) do
+  #   {[], %{state | stream_head: [frame]}}
+  # end
+  # def handle_frame(frame = %Continuation{fin: true}, state) do
+  #   stream_head = state.
+  # end
+  # def handle_frame(frame = %Data{}, state) do
+  #   {:ok, pid} = fetch_stream(frame, state)
+  #   Stream.send_data(pid, frame)
+  # end
+  #
+  # def start_stream(head = {:GET, "/foo", _ip}) do
+  #   # start under dynamic supervisor
+  #   Ace.FooController.start_link(head)
+  #   {:ok, pid} = start_link(Ace.FooController, :init, [[frame]])
+  #
+  # end
 
 
 end
--- test/ace_test.exs
+++ test/ace_test.exs
@@ -1,9 +1,42 @@
 defmodule AceTest do
   use ExUnit.Case
+
   setup do
-    # Setup a server
-    {:ok, %{port: :port}}
+    certfile =  Path.expand("./ace/tls/cert.pem", __DIR__)
+    keyfile =  Path.expand("./ace/tls/key.pem", __DIR__)
+    options = [
+      active: false,
+      mode: :binary,
+      packet: :raw,
+      certfile: certfile,
+      keyfile: keyfile,
+      reuseaddr: true,
+      alpn_preferred_protocols: ["h2", "http/1.1"]
+    ]
+    {:ok, listen_socket} = :ssl.listen(0, options)
+    {:ok, server} = Ace.start_link(listen_socket)
+    {:ok, {_, port}} = :ssl.sockname(listen_socket)
+    {:ok, connection} = :ssl.connect('localhost', port, [
+      mode: :binary,
+      packet: :raw,
+      active: :true,
+      alpn_advertised_protocols: ["h2"]])
+      :ssl.negotiated_protocol(connection)
+    {:ok, %{client: connection}}
   end
+
+  test "a single get request", %{client: connection} do
+    payload = [
+      Ace.preface(),
+      <<0::24, 4::8, 0::8, 0::32>>,
+    ]
+    :ssl.send(connection, payload)
+    assert_receive 5, 1_000
+    Process.sleep(5_000)
+  end
+
+
+
   @preface "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 
   def parse_frame(<<length :: 24, type :: 8, flags :: 8, 0 :: 1, id :: 31>> <> rest) do
@@ -49,6 +82,7 @@ defmodule AceTest do
     end
   end
 
+  @tag :skip
   test "get a single response" do
     preface = @preface
     Application.ensure_all_started(:crypto)
