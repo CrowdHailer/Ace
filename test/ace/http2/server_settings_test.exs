8ce25d96af0c1fca4c7a93270bda54e59d85984a
CrowdHailer <peterhsaxton@gmail.com>
2017-07-16 08:08:38 +0100

    add server test for settings

---
+++ test/ace/http2/server_settings_test.exs
@@ -0,0 +1,49 @@
+defmodule Ace.HTTP2.ServerSettingsTest do
+  use ExUnit.Case
+
+  setup do
+    certfile =  Path.expand("../../ace/tls/cert.pem", __DIR__)
+    keyfile =  Path.expand("../../ace/tls/key.pem", __DIR__)
+    options = [
+      active: false,
+      mode: :binary,
+      packet: :raw,
+      certfile: certfile,
+      keyfile: keyfile,
+      reuseaddr: true,
+      alpn_preferred_protocols: ["h2", "http/1.1"]
+    ]
+    {:ok, listen_socket} = :ssl.listen(0, options)
+    {:ok, _server} = Ace.HTTP2.start_link(listen_socket, {__MODULE__, %{test_pid: self()}})
+    {:ok, {_, port}} = :ssl.sockname(listen_socket)
+    {:ok, connection} = :ssl.connect('localhost', port, [
+      mode: :binary,
+      packet: :raw,
+      active: :false,
+      alpn_advertised_protocols: ["h2"]])
+      :ssl.negotiated_protocol(connection)
+    payload = [
+      Ace.HTTP2.preface(),
+      Ace.HTTP2.Frame.Settings.new() |> Ace.HTTP2.Frame.Settings.serialize(),
+    ]
+    :ssl.send(connection, payload)
+    :ssl.recv(connection, 9)
+    assert {:ok, %Ace.HTTP2.Frame.Settings{ack: true}} == Support.read_next(connection)
+    {:ok, %{client: connection}}
+  end
+
+  def route(_) do
+    __MODULE__
+  end
+
+  alias Ace.HTTP2.Frame
+
+  # TODO diff given settings with defaults and only send changed
+  # TODO start server with a max frame size and check it is sent to client and reflected in behaviour
+  test "max frame size cannot be", %{client: connection} do
+  end
+
+  # send incorrectly sized rst_stream frame
+
+
+end
