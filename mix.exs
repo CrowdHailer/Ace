871420719d8efd128e9aa514fc48283bb56aa4f8
Sam Davies <sampdavies@gmail.com>
2017-08-16 14:26:41 +0100

    Discard down message with a different ref

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ace.Mixfile do
 
   def project do
     [app: :ace,
-    version: "0.9.2",
+    version: "0.9.3",
     elixir: "~> 1.4",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
