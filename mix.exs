25f03162a5dfbb6cb3c0159009a40b096a8db023
CrowdHailer <peterhsaxton@gmail.com>
2018-07-15 16:32:41 +0100

    release bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ace.Mixfile do
   def project do
     [
       app: :ace,
-      version: "0.16.6",
+      version: "0.16.7",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
